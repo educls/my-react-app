5d41814aa23acc2af719b921c45053af59eececb
Educls <3duardocesar@gmail.com>
2024-08-01 21:37:39 -0300

    add: concord video

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './css/Projects.css';
 import './css/ButtonSeeMore.css'
 import printUnreal from '../assets/print-unreal.png'
 import printConcord from '../assets/print-concord.png'
+
 import videoUnreal from '../assets/record-unreal.mp4'
+import videoConcord from '../assets/record-concord.mp4'
 
 import VideoModal from './ModalVideo';
 
@@ -19,13 +21,16 @@ const Projects = () => {
             explorar a ferramenta e aprender.`;
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [videoSrc, setVideoSrc] = useState('');
 
-  const openModal = () => {
+  const openModal = (src) => {
+    setVideoSrc(src);
     setModalIsOpen(true);
   };
 
   const closeModal = () => {
     setModalIsOpen(false);
+    setVideoSrc('');
   };
 
 
@@ -44,38 +49,37 @@ const Projects = () => {
           <img src={printConcord} alt="" />
 
           <div>
-            <button class="learn-more">
+            <button class="learn-more" onClick={() => openModal(videoConcord)}>
               <span aria-hidden="true" class="circle">
                 <span class="icon arrow"></span>
               </span>
               <span class="button-text">Veja mais</span>
             </button>
           </div>
-
         </div>
         <div
           className="project-item"
-          data-aos="fade-left"
+          data-aos="fade-right"
           data-aos-delay="200"
         >
           <h3>Unreal Engine game</h3>
           <p>{descriptionUnreal}</p>
           <img src={printUnreal} alt="" />
           <div>
-            <button class="learn-more" onClick={openModal}>
+            <button class="learn-more" onClick={() => openModal(videoUnreal)}>
               <span aria-hidden="true" class="circle">
                 <span class="icon arrow"></span>
               </span>
               <span class="button-text">Veja mais</span>
             </button>
-            <VideoModal
-              isOpen={modalIsOpen}
-              onRequestClose={closeModal}
-              videoSrc={videoUnreal}
-            />
           </div>
         </div>
       </div>
+      <VideoModal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        videoSrc={videoSrc}
+      />
     </section>
   );
 }
